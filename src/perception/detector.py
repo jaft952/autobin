b343c99cd8069d4e5439c785048218e9d5cc8913
rikashi-shifu <harryliow229@gmail.com>
2026-06-30 14:09:39 +0800

    test: 01072026 model

--- src/perception/detector.py
+++ src/perception/detector.py
@@ -180,7 +180,7 @@ class AluminiumCanDetector:
         detector.stop()
     """
 
-    DEFAULT_MODEL_PATH = "src/models/best.pt"
+    DEFAULT_MODEL_PATH = "src/models/inference_01072026.pt"
 
     def __init__(
         self,
