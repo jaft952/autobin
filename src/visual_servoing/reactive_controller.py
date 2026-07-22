3535f80fb97f95b646c925e7b2645c2c0b144acd
rikashi-shifu <harryliow229@gmail.com>
2026-07-23 06:02:49 +0800

    chore: tune back to 20

--- src/visual_servoing/reactive_controller.py
+++ src/visual_servoing/reactive_controller.py
@@ -18,9 +18,9 @@ from typing import Optional
 from src.visual_servoing.distance_error import TargetError, STOP_DISTANCE_CM
 from src.motion.differential_kinematics import DifferentialKinematics, WheelCommand
 
-MAX_SPEED = 18.0             # TODO tune: matches MotionCalibration's default arc_speed
-FALLBACK_SPEED = 18.0        # TODO tune: used only when distance can't be estimated at all
-BACKUP_SPEED = 18.0          # TODO tune: gentle reverse to recover from an overshoot
+MAX_SPEED = 20.0             # TODO tune: matches MotionCalibration's default arc_speed
+FALLBACK_SPEED = 20.0        # TODO tune: used only when distance can't be estimated at all
+BACKUP_SPEED = 20.0          # TODO tune: gentle reverse to recover from an overshoot
 MAX_STEER_ANGLE_DEG = 50.0   # keep below 90 so it never fully spins in place
 FAR_DISTANCE_CM = 30.0       # distance at/beyond which speed is MAX_SPEED
 
