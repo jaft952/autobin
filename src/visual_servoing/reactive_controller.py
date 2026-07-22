db37e52c33984d9f9a701b8981c427c511d4bb7e
rikashi-shifu <harryliow229@gmail.com>
2026-07-23 06:31:29 +0800

    chore: tune max speed to 24

--- src/visual_servoing/reactive_controller.py
+++ src/visual_servoing/reactive_controller.py
@@ -24,9 +24,9 @@ from src.motion.differential_kinematics import DifferentialKinematics, WheelComm
 # placeholder (see that module), so a smooth ramp off it is no more
 # trustworthy than a few hardcoded checkpoints tuned by eye on hardware.
 CRUISE_DISTANCE_CM = 90.0    # distance at/beyond which speed is MAX_SPEED
-MAX_SPEED = 25.0             # TODO tune: hand-tested cruise speed at/beyond CRUISE_DISTANCE_CM
+MAX_SPEED = 24.0             # TODO tune: hand-tested cruise speed at/beyond CRUISE_DISTANCE_CM
 APPROACH_SPEED = 20.0        # TODO tune: hand-tested speed between FAR_DISTANCE_CM and CRUISE_DISTANCE_CM
-FALLBACK_SPEED = 24.0        # TODO tune: used only when distance can't be estimated at all
+FALLBACK_SPEED = 20.0        # TODO tune: used only when distance can't be estimated at all
 
 # Lowered from 24 (same as MAX_SPEED) -- on hardware that drove the
 # too_close backup at full speed for as long as too_close stayed True,
