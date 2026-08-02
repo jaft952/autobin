8375c925e4f7a3618472fde55b4e44cdf4b19eed
rikashi-shifu <harryliow229@gmail.com>
2026-08-02 14:48:48 +0800

    chore :caliebrate home and ttb angles

--- src/arm/arm_settings.py
+++ src/arm/arm_settings.py
@@ -11,10 +11,10 @@ from typing import Optional
 
 from src.hardware.actuators.pca9685_driver import clamp_channel_angle
 
-HOME_ANGLES     = [100.0, 30.0, 0.0, 150.0, 90.0]
-BIN_DROP_ANGLES = [96.7, 96.7, 100.0, 20.0, 90.0]
+HOME_ANGLES     = [100.0, 100.0, 170.0, 180.0, 80.0]
+BIN_DROP_ANGLES = [100.0, 100.0, 85.0, 10.0, 80.0]
 GRAB_ANGLES     = [101.0, 106.0, 40.0, 180.0, 80.0]
-ARC_LIFT_ARM    = [96.7, 96.7, 100.0, 100.0, 90.0]
+ARC_LIFT_ARM    = [96.7, 96.7, 100.0, 100.0, 80.0]
 
 GRIPPER_OPEN = 50.0
 GRIPPER_CLOSED = 4.0
