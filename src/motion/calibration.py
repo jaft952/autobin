16922737051b4a2a50842bf5b74a7c9bdeaaa3c9
rikashi-shifu <harryliow229@gmail.com>
2026-08-03 11:10:07 +0800

    test: calibrate forward and backward

--- src/motion/calibration.py
+++ src/motion/calibration.py
@@ -25,9 +25,9 @@ class MotionCalibration:
     arc_speed: float = 90.0
 
     motor_a_forward_trim: float = 1.0 # right
-    motor_b_forward_trim: float = 0.8 # left
-    motor_a_backward_trim: float = 0.95 # right
-    motor_b_backward_trim: float = 1.0 # left
+    motor_b_forward_trim: float = 0.75 # left
+    motor_a_backward_trim: float = 1.0 # right
+    motor_b_backward_trim: float = 0.9 # left
     motor_a_turn_trim: float = 1.0
     motor_b_turn_trim: float = 1.0
 
