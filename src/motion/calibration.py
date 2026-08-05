7839cfaa66fc9d2b8e0156eb5eeb6435374d54a8
rikashi-shifu <harryliow229@gmail.com>
2026-08-05 12:35:29 +0800

    test: calibrate motion

--- src/motion/calibration.py
+++ src/motion/calibration.py
@@ -26,7 +26,7 @@ class MotionCalibration:
 
     motor_a_forward_trim: float = 1.0 # right
     motor_b_forward_trim: float = 0.95 # left
-    motor_a_backward_trim: float = 0.85 # right
+    motor_a_backward_trim: float = 0.88 # right
     motor_b_backward_trim: float = 1.0 # left
     motor_a_turn_trim: float = 1.0
     motor_b_turn_trim: float = 1.0
