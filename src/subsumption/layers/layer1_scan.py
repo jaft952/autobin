04707ed2faec5bf5bdf9606f5968e381ff4a06e7
rikashi-shifu <harryliow229@gmail.com>
2026-09-03 02:11:18 +0800

    chore: ignore type error

--- src/subsumption/layers/layer1_scan.py
+++ src/subsumption/layers/layer1_scan.py
@@ -262,7 +262,7 @@ class ScanAroundLayer(BaseLayer):
                 self._enter(self._next_phase, now)
 
         elif self._phase == _Phase.DRIVE:
-            if wall_ahead and front <= BACKOFF_AT_CM:
+            if wall_ahead and front <= BACKOFF_AT_CM: # type: ignore
                 self._settle(_Phase.BACKOFF, now)
             elif wall_ahead:
                 self._start_dodge(sensors, now)
