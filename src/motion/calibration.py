8d4e808d6d9e043c2e0e6906bf369b386832ba6f
rikashi-shifu <harryliow229@gmail.com>
2026-09-10 13:51:38 +0800

    chore: calibrate motor

--- src/motion/calibration.py
+++ src/motion/calibration.py
@@ -19,7 +19,7 @@ class MotionCalibration:
     arc_speed: float = 90.0
 
     motor_a_forward_trim: float = 1.0 # right
-    motor_b_forward_trim: float = 1.0 # left
+    motor_b_forward_trim: float = 0.5 # left
     motor_a_backward_trim: float = 1.0 # right
     motor_b_backward_trim: float = 0.95 # left
     motor_a_turn_trim: float = 1.0
